5a0e6b4d2d1ecab52acd3bb62b06d991c0848710
Ishan200207 <168297757+Ishan200207@users.noreply.github.com>
2025-07-03 23:36:19 +0530

    Auth Routes Design

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./db/connectDB.js";
+import authRoutes from "./routes/authRoute.js";
 
 dotenv.config();
 const app = express();
@@ -8,6 +9,8 @@ const app = express();
 app.get("/", (req, res)=>(
     res.send("Hello World!")
 ));
+
+app.use("/api/auth", authRoutes);
 
 app.listen(3000, ()=>{
     connectDB();
