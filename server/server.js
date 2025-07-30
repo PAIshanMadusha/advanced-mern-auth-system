a4dc3e453b43108b621773c3b09a7653632f4459
Ishan200207 <168297757+Ishan200207@users.noreply.github.com>
2025-07-30 20:46:12 +0530

    server.js File Configuration

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import path from "path";
+import { fileURLToPath } from "url";
 import { connectDB } from "./db/connectDB.js";
 import authRoutes from "./routes/authRoute.js";
 import cors from "cors";
@@ -10,22 +11,34 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const __dirname = path.resolve();
 
-app.use(cors({origin: "http://localhost:5173", credentials: true}));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+if (process.env.NODE_ENV === "development") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+      credentials: true,
+    })
+  );
+}
+
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "/client/dist")));
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
-    });
+if (process.env.NODE_ENV === "production") {
+  const clientDistPath = path.join(__dirname, "..", "client", "dist");
+
+  app.use(express.static(clientDistPath));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(clientDistPath, "index.html"));
+  });
 }
 
-app.listen(PORT, ()=>{
-    connectDB();
-    console.log("Server is Running on Port: ", PORT);
+app.listen(PORT, () => {
+  connectDB();
+  console.log("Server is running on port:", PORT);
 });
